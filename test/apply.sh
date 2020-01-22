4def7361f6aa06e75044d7f9b091604d4fd29363
Ankita Thomas <ankthoma@redhat.com>
2020-01-22 17:32:08 -0500

    4.2

--- test/apply.sh
+++ test/apply.sh
@@ -3,7 +3,7 @@ if [ -f ./release-head.txt ]; then
 	j=$(cat release-head.txt)
 fi
 
-j=$(( ( $j + 1 ) % 2 ));
+j=$(( ( $j + 1 ) % 4 ));
 oc apply -n release-controller-test-release -f "release-$j.yaml";
 
 echo $j > release-head.txt
